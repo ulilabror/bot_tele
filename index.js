d50bc08d9e6d6fb71fbea021b2d3ab5024212d4b
mulila <root087732168347@gmail.com>
2020-10-04 15:54:55 +0700

    bot_tele

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const Slimbot = require('slimbot');
-const slimbot = new Slimbot('1182878039:AAE2Px8NwUMqp4JUp3utJayXF5ht4Y5r2Z8');
- 
-// Register listeners
- 
-slimbot.on('message', message => {
-    console.log(message)
-  slimbot.sendMessage(message.chat.id, 'Message received');
-});
- 
-// Call API
- 
-slimbot.startPolling();
+require('./bot');
+require('./web');
