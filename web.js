865ca9ac8d71bbd42f4d6fe8627284ea367a04a2
mulila <root087732168347@gmail.com>
2020-10-05 14:20:00 +0700

    bot_tele

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var packageInfo = require('./package.json');
-
+var moment = require('moment-timezone');
 var app = express();
-
+var time = moment().tz("Asia/Jakarta").format('h:mm:ss a')
 app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
+  res.json({ version: packageInfo.version,timenow: time});
 });
 
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Web server started at http://%s:%s', host, port);
+  console.log('Web server started at http://%s:%s time now:'+time , host, port);
 });
